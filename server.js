a9691ae340648658ca8cde3a525eb3a7eeff93d9
Daniel Devine <danieldevine@daniels-mbp.home>
2020-09-04 07:44:22 +0100

    working server

--- server.js
+++ server.js
@@ -1,32 +1,35 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
-const PORT = process.env.PORT || 3000;
-const app = express()
+
+const port = process.env.PORT || 4001;
+const index = require("./routes/index");
+
+const app = express();
+app.use(index);
+
 const server = http.createServer(app);
 
 const io = socketIo(server);
 
-const rooms = {};
+let interval;
 
-io.on("connection", socket => {
-    socket.on("join room", roomID => {
-        if (rooms[roomID]) {
-            rooms[roomID].push(socket.id);
-        } else {
-            rooms[roomID] = [socket.id];
-        }
-        const otherUser = rooms[roomID].find(id => id !== socket.id);
-        if (otherUser) {
-            socket.emit("other user", otherUser);
-            socket.to(otherUser).emit("user joined", socket.id);
-        }
-    });
-
-    socket.on("offer", payload => io.to(payload.target).emit("offer", payload))
-    socket.on("answer", payload =>  io.to(payload.target).emit("answer", payload))
-    socket.on("ice-candidate", incoming => io.to(incoming.target).emit("ice-candidate", incoming.candidate))
+io.on("connection", (socket) => {
+  console.log("New client connected");
+  if (interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(() => getApiAndEmit(socket), 1000);
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+    clearInterval(interval);
+  });
 });
 
+const getApiAndEmit = socket => {
+  const response = new Date();
+  // Emitting a new message. Will be consumed by the client
+  socket.emit("FromAPI", response);
+};
 
-server.listen(PORT, () => console.log('server is running on port 8000'));
+server.listen(port, () => console.log(`Listening on port ${port}`));
