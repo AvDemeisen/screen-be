0d90bb8c0181e2856dc84346e945aa6dd6e3a7c7
Daniel Devine <danieldevine@daniels-mbp.home>
2020-09-04 08:03:45 +0100

    potentially working

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
-
 const port = process.env.PORT || 4001;
 const index = require("./routes/index");
-
 const app = express();
 app.use(index);
-
 const server = http.createServer(app);
-
 const io = socketIo(server);
-
+const rooms = {};
 let interval;
 
 io.on("connection", (socket) => {
@@ -24,6 +20,23 @@ io.on("connection", (socket) => {
     console.log("Client disconnected");
     clearInterval(interval);
   });
+
+  socket.on("join room", roomID => {
+    if (rooms[roomID]) {
+        rooms[roomID].push(socket.id);
+    } else {
+        rooms[roomID] = [socket.id];
+    }
+    const otherUser = rooms[roomID].find(id => id !== socket.id);
+    if (otherUser) {
+        socket.emit("other user", otherUser);
+        socket.to(otherUser).emit("user joined", socket.id);
+    }
+});
+
+socket.on("offer", payload => io.to(payload.target).emit("offer", payload))
+socket.on("answer", payload =>  io.to(payload.target).emit("answer", payload))
+socket.on("ice-candidate", incoming => io.to(incoming.target).emit("ice-candidate", incoming.candidate))
 });
 
 const getApiAndEmit = socket => {
