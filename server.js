860e5274afcaf94f348a69ef0f7ddee6ffa4292d
Daniel Devine <danieldevine@daniels-mbp.home>
2020-09-08 09:52:35 +0100

    commit 0222

--- server.js
+++ server.js
@@ -1,48 +1,42 @@
+require('dotenv').config();
 const express = require("express");
 const http = require("http");
-const socketIo = require("socket.io");
-const port = process.env.PORT || 4001;
-const index = require("./routes/index");
 const app = express();
-app.use(index);
 const server = http.createServer(app);
-const io = socketIo(server);
-const rooms = {};
-let interval;
+const socket = require("socket.io");
+const io = socket(server);
 
-io.on("connection", (socket) => {
-  console.log("New client connected");
-  if (interval) {
-    clearInterval(interval);
-  }
-  interval = setInterval(() => getApiAndEmit(socket), 1000);
-  socket.on("disconnect", () => {
-    console.log("Client disconnected");
-    clearInterval(interval);
-  });
+const users = {};
+const socketToRoom = {};
 
-  socket.on("join room", roomID => {
-    if (rooms[roomID]) {
-        rooms[roomID].push(socket.id);
-    } else {
-        rooms[roomID] = [socket.id];
-    }
-    const otherUser = rooms[roomID].find(id => id !== socket.id);
-    if (otherUser) {
-        socket.emit("other user", otherUser);
-        socket.to(otherUser).emit("user joined", socket.id);
-    }
+io.on('connection', socket => {
+    socket.on("join room", roomID => {
+        if (users[roomID]) {
+            const length = users[roomID].length;
+            if (length === 4) {
+                socket.emit("room full");
+                return;
+            }
+            users[roomID].push(socket.id);
+        } else users[roomID] = [socket.id];
+        
+        socketToRoom[socket.id] = roomID;
+        const usersInThisRoom = users[roomID].filter(id => id !== socket.id);
+        socket.emit("all users", usersInThisRoom);
+    });
+
+    socket.on("sending signal", payload =>  io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID }));
+    socket.on("returning signal", payload => io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id }));
+
+    socket.on('disconnect', () => {
+        const roomID = socketToRoom[socket.id];
+        let room = users[roomID];
+        if (room) {
+            room = room.filter(id => id !== socket.id);
+            users[roomID] = room;
+        }
+    });
+
 });
 
-socket.on("offer", payload => io.to(payload.target).emit("offer", payload))
-socket.on("answer", payload =>  io.to(payload.target).emit("answer", payload))
-socket.on("ice-candidate", incoming => io.to(incoming.target).emit("ice-candidate", incoming.candidate))
-});
-
-const getApiAndEmit = socket => {
-  const response = new Date();
-  // Emitting a new message. Will be consumed by the client
-  socket.emit("FromAPI", response);
-};
-
-server.listen(port, () => console.log(`Listening on port ${port}`));
+server.listen(process.env.PORT || 4001, () => console.log('server is running on port 4000'));
